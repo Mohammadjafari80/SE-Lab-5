22864997aec44e9d264d9cb10a25626b22795d32
Mohammad Jafari <mamad.jafari91@gmail.com>
2023-08-19 10:21:19 +0330

    add optimized Fibonacci

--- src/FibonacciCalculator.java
+++ src/FibonacciCalculator.java
@@ -1,22 +1,32 @@
+import java.util.Scanner;
+
 public class FibonacciCalculator {
 
     public static void main(String[] args) {
-        int n = 50;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Press N: ");
+        int n = scanner.nextInt();
 
         // Recursive method
         long startTimeRecursive = System.currentTimeMillis();
         int resultRecursive = fibonacci(n);
         long endTimeRecursive = System.currentTimeMillis();
 
-        System.out.println("Recursive Fibonacci(" + n + ") = " + resultRecursive);
-        System.out.println("Time taken (Recursive): " + (endTimeRecursive - startTimeRecursive) + " ms");
+        System.out.println("Iterative Fibonacci(" + n + ") = " + resultRecursive);
+        System.out.println("Time taken (Iterative): " + (endTimeRecursive - startTimeRecursive) + " ms");
     }
 
-    // Recursive Fibonacci method (inefficient)
     public static int fibonacci(int n) {
         if (n <= 1) {
             return n;
         }
-        return fibonacci(n - 1) + fibonacci(n - 2);
+        int fib = 1;
+        int prevFib = 1;
+        for (int i = 2; i < n; i++) {
+            int temp = fib;
+            fib += prevFib;
+            prevFib = temp;
+        }
+        return fib;
     }
 }
